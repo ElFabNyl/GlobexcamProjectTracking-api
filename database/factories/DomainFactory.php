98b97bb49db135f4840c97a53476f723beaebedc
StevyMarlino <stevyabessolo@gmail.com>
2021-05-04 20:32:40 +0200

    add factory
    Domain and user

---
+++ database/factories/DomainFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Domain;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+class DomainFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Domain::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name_host' => Arr::random(['stevy.com','yvan.net','joe.cm','nyl.fr']),
+            'name_customer' => $this->faker->name,
+            'price' => Arr::random(['15000','20000','25000','30000','40000','50000']),
+            'service' => Arr::random(['RENEW','REGISTER','SMS']),
+            'method_payment' => Arr::random(['ORANGE MONEY','MTN MONEY','CASH']),
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -24,9 +25,12 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'last_name' => $this->faker->lastName(),
+            'phone' => $this->faker->phone(),
+            'role' => Arr::random(['admin','accountant','user','manager','customer']),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
     }
