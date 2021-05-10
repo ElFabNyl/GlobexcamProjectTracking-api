ee3d64b45508c4a9d5b1f52ca531c4602fc000d3
StevyMarlino <stevyabessolo@gmail.com>
2021-05-10 19:57:52 +0200

    Update ProjetController.php
    creation du controller api pour le model projet

--- app/Http/Controllers/ProjetController.php
+++ app/Http/Controllers/ProjetController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Projet\ProjetStoreRequest;
+use App\Http\Requests\Projet\ProjetUpdateRequest;
+use App\Http\Requests\ProjetRequest;
+use App\Models\Projet;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
+/**
+ * Class ProjetController
+ * @package App\Http\Controllers
+ */
 class ProjetController extends Controller
 {
-    //
+
+    /**
+     * ProjetController constructor.
+     */
+    public function __construct()
+    {
+        return $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        return Response::json([
+            'status' => true,
+            'data' => Projet::query()->select('*')->paginate(10),
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param ProjetStoreRequest $request
+     * @return void
+     */
+    public function store(ProjetStoreRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Projet $projet
+     * @return void
+     */
+    public function show(Projet $projet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param ProjetUpdateRequest $request
+     * @param Projet $projet
+     * @return void
+     */
+    public function update(ProjetUpdateRequest $request, Projet $projet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Projet $projet
+     * @return void
+     */
+    public function destroy(Projet $projet)
+    {
+        //
+    }
 }
