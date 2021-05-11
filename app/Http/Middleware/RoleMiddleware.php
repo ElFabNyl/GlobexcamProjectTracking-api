108c7eb04f5e268d2757e0503bb4556847de8678
StevyMarlino <stevyabessolo@gmail.com>
2021-05-11 12:40:18 +0200

    Create RoleMiddleware.php
    ajout d'un middleware de control des roles acces au ressource ou endpoint requis

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @param array $role
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, ... $role)
+    {
+        //Exception Si l'utilisateur n'est pas connecte
+
+        if(!Auth::check())
+        {
+            return Response::json([
+                'status' => false,
+                'message' => 'Vous devez vous authentifier pour acceder a cette page'
+            ],403);
+        }
+
+        //Handle Access denied exception if the roles are not in roles array
+
+        if(!in_array(Auth::user()->role, $role))
+        {
+            return Response::json([
+                'status' => false,
+                'message' => 'Vous n\' avez pas les droits d\'acces a cette page'
+            ],403);
+        }
+
+        //The request continue
+        return $next($request);
+    }
+}
