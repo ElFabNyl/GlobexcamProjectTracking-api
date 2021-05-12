7abe20c9404de776301d4c9cd83762ee70e330b1
StevyMarlino <stevyabessolo@gmail.com>
2021-05-12 11:25:35 +0200

    Update Projet.php

--- app/Models/Projet.php
+++ app/Models/Projet.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Projet extends Model
 {
@@ -35,5 +36,13 @@ class Projet extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function findBySlug($slug)
+    {
+      return  DB::table('projets')
+          ->select('*')
+          ->where('slug','=',$slug)
+          ->first();
     }
 }
