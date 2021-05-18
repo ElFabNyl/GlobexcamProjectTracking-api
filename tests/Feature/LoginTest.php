ed773631158056bd7537052847e94bed89f2d321
StevyMarlino <stevyabessolo@gmail.com>
2021-05-18 17:41:05 +0200

    Create LoginTest.php
    test Login

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * @return void
+     * @test
+     */
+    public function requires_email_and_password()
+    {
+        $this->json('POST', 'api/user/login')
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "validation error",
+                "error" => [
+                    "email" => [
+                        "The email field is required."
+                    ],
+                    "password" => [
+                        "The password field is required."
+                    ]
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function require_email()
+    {
+        $this->json('POST', 'api/user/login')
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "validation error",
+                "error" => [
+                    "email" => [
+                        "The email field is required."
+                    ]
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function require_password()
+    {
+        $this->json('POST', 'api/user/login')
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "validation error",
+                "error" => [
+                    "password" => [
+                        "The password field is required."
+                    ]
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function user_login_successfully()
+    {
+        $user = User::factory()->create([
+            'email' => 'stevymarlino@user.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $payload = ['email' => 'stevymarlino@user.com', 'password' => 'password'];
+
+        $this->json('POST', 'api/user/login', $payload)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'message',
+                'data' => [
+                    'token',
+                    'user' => [
+                        'id',
+                        'name',
+                        'last_name',
+                        'phone',
+                        'role',
+                        'email',
+                        'email_verified_at',
+                        'created_at',
+                        'updated_at',
+                    ]
+                ],
+            ]);
+    }
+
+}
